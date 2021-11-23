b70bb071f41e68c8e71efdc368f9f6f4334072d4
caiohscruz <caiohscruz@hotmail.com>
2021-11-23 20:20:19 -0300

    setting authentication

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,15 +1,30 @@
+import { AuthenticationService } from './../../authentication/authentication.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor(private authService: AuthenticationService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  username: string = '';
+  password: string = '';
+
+  login() {
+    this.authService.authenticate(this.username, this.password)
+      .subscribe(
+        ()=> {
+          console.log("Ok")
+        },
+        (error) =>{
+          alert("Credenciais inválidas")
+          console.log(error)
+        }
+      )
+
   }
-
 }
