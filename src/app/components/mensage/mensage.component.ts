df696c6048feb73ef4cbe733eee61d17ba4039c6
caiohscruz <caiohscruz@hotmail.com>
2021-11-25 18:53:44 -0300

    create mensage component

---
+++ src/app/components/mensage/mensage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mensage',
+  templateUrl: './mensage.component.html',
+  styleUrls: ['./mensage.component.css']
+})
+export class MensageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
